0821b9352f0ed3c4057f106aa32e4f3029cf3230
Fisher <dennisfusion2@gmail.com>
2023-09-19 05:50:46 -0300

    components dinamycs propertys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
@@ -11,7 +10,6 @@ import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent,
     MenuTitleComponent,
     SmallCardComponent,
     BigCardComponent,
